48ef6eb8c056ddb07aacf10f653ab9ef9173deb1
Sawyer Mifsud <wingren013@gmail.com>
2017-05-08 19:04:52 -0700

    preliminary bh code

--- physics_checker/src/barnes_hut.c
+++ physics_checker/src/barnes_hut.c
@@ -6,12 +6,13 @@
 /*   By: smifsud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/08 15:00:12 by smifsud           #+#    #+#             */
-/*   Updated: 2017/05/08 17:34:09 by smifsud          ###   ########.fr       */
+/*   Updated: 2017/05/08 19:02:50 by smifsud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <checker.h>
+#include <pthread.h>
 
 #define UNIVERSE_START(X) universe->children[X]->start
 #define UNIVERSE_END(X) universe->children[X]->end
@@ -36,19 +37,52 @@ t_octant	*endtree(t_octant *universe, size_t prtc)
 	return (universe);
 }
 
-void		bh(t_octant *currentuniverse, t_octant *newuniverse)
+#define PARTICLE newuniverse->bodies[prtc]
+
+void		bh(t_octant *node, t_octant *newuniverse, size_t prtc)
 {
+	size_t	i;
+
+	i = node->parent->start;
+	//compare against all particles in current node and neighbor nodes
+	while (i < node->parent->end)
+	{
+		if (i == prtc)
+			continue ;
+		adjustvelocity(newuniverse, prtc, node->parent->bodies[i]);
+		i++;
+	}
+	//compare against all other nodes
+	while (1)
+	{
+		node = node->parent;
+		i = 0;
+		if (node == 0)
+			break ;
+		while (i < 8)
+		{
+			if (node->parent->children[i] != node)
+			adjustvelocity_nodes(newuniverse, prtc, node->parent->children[i]);
+			i++;
+		}
+	}
+	adjustposition(newuniverse, prtc); //adjust position based on velocity
 }
 
 t_octant	*barnes_hut(t_octant *universe)
 {
-	t_octant	*nextframe;
+	t_octant	*newuniverse;
 	size_t		i;
 
 	i = 0;
+	newuniverse->start = universe->start;
+	newuniverse->end = universe->end;
+	newuniverse->bodies = (t_body*)malloc(sizeof(t_body) * universe->end);
+	//multithread this
 	while (i < universe->end)
 	{
-		bh(endtree(universe, i), nextframe); 
+		newuniverse->bodies[i] = universe->bodies[i];
+		bh(endtree(universe, i), newuniverse, i);
 		i++;
 	}
 }
