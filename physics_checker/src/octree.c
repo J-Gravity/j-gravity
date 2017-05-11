330244eb18c1bd2730e3dae68a45ffd60cd482f4
Eung Bum Lee <eungbumlee@gmail.com>
2017-05-11 12:57:16 -0700

    changed up octree.c so that we dont have to store center of mass or physical center in t_octant struct

--- physics_checker/src/octree.c
+++ physics_checker/src/octree.c
@@ -6,7 +6,7 @@
 /*   By: elee <elee@student.42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/08 19:46:04 by elee              #+#    #+#             */
-/*   Updated: 2017/05/10 20:57:11 by elee             ###   ########.fr       */
+/*   Updated: 2017/05/11 12:56:02 by elee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,53 +29,43 @@ void        createchildren(t_octant *node)
     }
 }
 
-void        set_center_of_mass(t_octant *node)
+void        get_physical_center(t_octant *node)
 {
     size_t      i;
-    t_vector    com;
-	t_vector	com2;
 	t_vector	center;
 
-    com.x = 0.0;
-    com.y = 0.0;
-    com.z = 0.0;
-	com2.x = 0.0;
-	com2.y = 0.0;
-	com2.z = 0.0;
-    node->combinedmass = 0.0;
+	center.x = 0.0;
+	center.y = 0.0;
+	center.z = 0.0;
     i = node->start;
     while (i <= node->end)
     {
-        com.x += node->bodies[i].position.x * node->bodies[i].mass;
-        com.y += node->bodies[i].position.y * node->bodies[i].mass;
-        com.z += node->bodies[i].position.z * node->bodies[i].mass;
-		com2.x += node->bodies[i].position.x;
-		com2.y += node->bodies[i].position.y;
-		com2.z += node->bodies[i].position.z;
-        node->combinedmass += bodies[i].mass;
+		center.x += node->bodies[i].position.x;
+		center.y += node->bodies[i].position.y;
+		center.z += node->bodies[i].position.z;
         i++;
     }
-    node->centerofmass.x = com.x / node->combinedmass;
-    node->centerofmass.y = com.y / node->combinedmass;
-    node->centerofmass.z = com.z / node->combinedmass;
-	node->center.x = com2.x / (node->end - node->start + 1);
-	node->center.y = com2.y / (node->end - node->start + 1);
-	node->center.z = com2.z / (node->end - node->start + 1);
+	center.x /= (node->end - node->start + 1);
+	center.y /= (node->end - node->start + 1);
+	center.z /= (node->end - node->start + 1);
+	return (center);
 }
 
 void        find_octant(t_octant *cell)
 {
-    size_t  i;
-    double  x;
-    double  y;
-    double  z;
-    
+	t_vector	center;
+    size_t  	i;
+    double  	x;
+    double  	y;
+    double  	z;
+   
+	center = get_physical_center(node);
     i = cell->start;
     while (i <= cell->end)
     {
-        x = cell->bodies[i].position.x - cell->center.x;
-        y = cell->bodies[i].position.y - cell->center.y;
-        z = cell->bodies[i].position.z - cell->center.z;
+        x = cell->bodies[i].position.x - center.x;
+        y = cell->bodies[i].position.y - center.y;
+        z = cell->bodies[i].position.z - center.z;
         if (x >= 0.0 && y >= 0.0 && z >= 0.0)
             cell->bodies[i].octant = 1;
         else if (x < 0.0 && y >= 0.0 && z >= 0.0)
@@ -170,7 +160,6 @@ t_octant    *octree_divide(t_octant *root)
 
     if (root->end < root->start)
         return (NULL);
-    set_center_of_mass(root);
     createChildren(root);
     find_octant(root);
     qsort(root->bodies + (root->start) * sizeof(t_body), (root->end - root->start + 1), sizeof(t_body), compare_octant);
