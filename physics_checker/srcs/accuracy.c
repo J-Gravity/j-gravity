837a779e454aaf3b15617ef0f9eb81638b341070
Sawyer Mifsud <wingren013@gmail.com>
2017-05-26 16:47:41 -0700

    wtf

--- physics_checker/srcs/accuracy.c
+++ physics_checker/srcs/accuracy.c
@@ -6,7 +6,7 @@
 /*   By: smifsud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/24 18:12:27 by smifsud           #+#    #+#             */
-/*   Updated: 2017/05/26 16:24:08 by smifsud          ###   ########.fr       */
+/*   Updated: 2017/05/26 16:46:41 by smifsud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,24 @@ void			accuracy_check(int cpucomp, int gpucomp)
 	mpf_t		compare;
 	mpf_t		reg;
 	mpf_t		math;
-	size_t		run;
+	int64_t		run;
 
 	printf("|--------CALCULATING ACCURACY--------|\n");
+	read(cpucomp, &run, sizeof(int64_t));
 	run = 0;
-	mpf_init2(bignum, 256);
-	mpf_init2(compare, 256);
-	mpf_init2(math, 256);
+	mpf_init2(bignum, 512);
+	mpf_init2(compare, 512);
+	mpf_init2(math, 512);
 	mpf_init2(reg, 64);
 	mpf_set_d(bignum, 0);
 	mpf_set_d(compare, 0);
 	printf("CONFIRM\n");
-	while (read(cpucomp, &buf, sizeof(t_vector) * 3))
+	while (read(cpucomp, &(buf), sizeof(t_vector)))
 	{
-		printf("RUN: %ld\n", run);
+		printf("RUN: %lld\n", run);
+		printf("%.40lf, %.40lf, %lf, %lf, %lf\n", mpf_get_d(compare), finddist(buf), buf.x, buf.y, buf.z);
 		mpf_set_d(reg, finddist(buf));
+		printf("paus\n");
 		mpf_add(compare, compare, reg);
 		run++;
 		if (1 > read(cpucomp, &buf, sizeof(double)))
@@ -46,11 +49,12 @@ void			accuracy_check(int cpucomp, int gpucomp)
 	}
 	printf("CALCULATED CPU SAMPLESET\n\n\n");
 	mpf_div_ui(compare, compare, run);
+	read(gpucomp, &run, sizeof(int64_t));
 	run = 0;
 	printf("DEBUGGY\n");
 	while (read(gpucomp, &cmpbuf, sizeof(t_fvector)))
 	{
-		printf("RUN: %ld\n", run);
+		printf("RUN: %lld\n", run);
 		buf.x = cmpbuf.x;
 		buf.y = cmpbuf.y;
 		buf.z = cmpbuf.z;
