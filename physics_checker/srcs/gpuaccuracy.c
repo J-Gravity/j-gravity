39f36aa345931f9acb86740bd62ae80c65249750
Sawyer Mifsud <wingren013@gmail.com>
2017-06-02 16:47:21 -0700

    clean up

--- physics_checker/srcs/gpuaccuracy.c
+++ physics_checker/srcs/gpuaccuracy.c
@@ -6,7 +6,7 @@
 /*   By: smifsud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/24 18:12:27 by smifsud           #+#    #+#             */
-/*   Updated: 2017/06/02 16:41:29 by smifsud          ###   ########.fr       */
+/*   Updated: 2017/06/02 16:47:12 by smifsud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,68 +128,6 @@ void			accuracy_check_gpu(int cpucomp, int gpucomp)
 	mpf_clear(temp);
 }
 
-/*
- * void			accuracy_check(int cpucomp, int gpucomp)
- * {
- * 	t_vector	buf;
- * 		t_fvector	cmpbuf;
- * 			mpf_t		bignum;
- * 				mpf_t		compare;
- * 					mpf_t		reg_x;
- * 						mpf_t		reg_y;
- * 							mpf_t		reg_z;
- * 								mpf_t		math;
- * 									int64_t		run;
- *
- * 										printf("|--------CALCULATING ACCURACY--------|\n");
- * 											read(cpucomp, &run, sizeof(int64_t));
- * 												run = 0;
- * 													mpf_init2(bignum, 512);
- * 														mpf_init2(compare, 512);
- * 															mpf_init2(math, 512);
- * 																mpf_init2(reg, 64);
- * 																	mpf_set_d(bignum, 0);
- * 																		mpf_set_d(compare, 0);
- * 																			printf("CONFIRM\n");
- * 																				while (read(cpucomp, &(buf), sizeof(t_vector)))
- * 																					{
- * 																						//	printf("RUN: %lld\n", run);
- * 																							//	printf("%.40lf, %.40lf, %lf, %lf, %lf\n", mpf_get_d(compare), finddist(buf), buf.x, buf.y, buf.z);
- * 																									mpf_set_d(reg, finddist(buf));
- * 																											printf("paus\n");
- * 																													mpf_add(compare, compare, reg);
- * 																															run++;
- * 																																}
- * 																																	printf("CALCULATED CPU SAMPLESET\n\n\n");
- * 																																		mpf_div_ui(compare, compare, run);
- * 																																			read(gpucomp, &run, sizeof(int64_t));
- * 																																				run = 0;
- * 																																					printf("DEBUGGY\n");
- * 																																						while (read(gpucomp, &cmpbuf, sizeof(t_fvector)))
- * 																																							{
- * 																																									printf("RUN: %lld\n", run);
- * 																																											buf.x = cmpbuf.x;
- * 																																													buf.y = cmpbuf.y;
- * 																																															buf.z = cmpbuf.z;
- * 																																																	mpf_set_d(reg, finddist(cmpbuf));
- * 																																																			mpf_add(bignum, bignum, reg);
- * 																																																					run++;
- * 																																																							if (1 > read(gpucomp, &cmpbuf, sizeof(float)))
- * 																																																										break ;
- * 																																																											}
- * 																																																												printf("CALCULATED GPU SAMPLESET\n\n\n");
- * 																																																													mpf_div_ui(bignum, bignum, run);
- * 																																																														mpf_add(math, bignum, compare);
- * 																																																															mpf_div_ui(math, math, 2);
- * 																																																																mpf_sub(bignum, bignum, compare);
- * 																																																																	mpf_abs(bignum, bignum);
- * 																																																																		mpf_div(bignum, bignum, math);
- * 																																																																			mpf_mul_ui(bignum, bignum, 100);
- * 																																																																				printf("DEVIATION : %lf%%\n", mpf_get_d(bignum));
- * 																																																																					mpf_clear(bignum);
- * 																																																																						mpf_clear(reg);
- * 																																																																						}
- * 																																																																						*/
 int			main(int argc, char **argv)
 {
 	accuracy_check_gpu(open(argv[1], O_RDONLY), open(argv[2], O_RDONLY));
